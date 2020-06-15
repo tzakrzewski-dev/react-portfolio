90d7040e559e1c1b34b5b3147514c5cfedaaff2f
Teddy Zakrzewski <teddy.zakrzewski@outlook.fr>
2020-06-15 22:53:40 +0200

    Updates

---
+++ precache-manifest.3d2de96a8c481f9ea8f2fa940d2b59f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6a8e559a7b29120ab3a7473c4ff517",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "65f1689a691abf0344d5",
+    "url": "/react-portfolio/static/css/2.fbf77e7b.chunk.css"
+  },
+  {
+    "revision": "9931aa6970b75bccd7f6",
+    "url": "/react-portfolio/static/css/main.9a973ff5.chunk.css"
+  },
+  {
+    "revision": "65f1689a691abf0344d5",
+    "url": "/react-portfolio/static/js/2.92b69128.chunk.js"
+  },
+  {
+    "revision": "9931aa6970b75bccd7f6",
+    "url": "/react-portfolio/static/js/main.e9ffa3bc.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  }
+]);
