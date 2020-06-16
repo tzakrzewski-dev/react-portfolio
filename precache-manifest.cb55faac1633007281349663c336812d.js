66868251c5f354ea0a6fe16edb9f096b6c1cbf9a
Teddy Zakrzewski <teddy.zakrzewski@outlook.fr>
2020-06-16 15:16:12 +0200

    Updates

---
+++ precache-manifest.cb55faac1633007281349663c336812d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54223586c4fba187705a3448f2590e83",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3b1bfe81f02ceb2880a0",
+    "url": "/react-portfolio/static/css/2.fbf77e7b.chunk.css"
+  },
+  {
+    "revision": "4a64f2836627b107e723",
+    "url": "/react-portfolio/static/css/main.4f478940.chunk.css"
+  },
+  {
+    "revision": "3b1bfe81f02ceb2880a0",
+    "url": "/react-portfolio/static/js/2.dd478fca.chunk.js"
+  },
+  {
+    "revision": "4a64f2836627b107e723",
+    "url": "/react-portfolio/static/js/main.cccdbf43.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.6740d815663244d76ba4679e7e1b7de5.js"
+  "/react-portfolio/precache-manifest.cb55faac1633007281349663c336812d.js"
 );
 
 self.addEventListener('message', (event) => {
