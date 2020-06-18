5d106fb484c1fb323e3edf9d36ca1672a89b5a87
Teddy Zakrzewski <teddy.zakrzewski@outlook.fr>
2020-06-18 17:10:38 +0200

    Updates

--- precache-manifest.49af1e23d2057ce2b1229eb352eeabf1.js
+++ precache-manifest.49af1e23d2057ce2b1229eb352eeabf1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "141226a06e6b10261273947905d5f6b3",
+    "revision": "c0083687865ef88654cc6011cb1c18c3",
     "url": "/react-portfolio/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/css/2.fbf77e7b.chunk.css"
   },
   {
-    "revision": "0b20932b29a01ec68f96",
-    "url": "/react-portfolio/static/css/main.bdd98e7f.chunk.css"
+    "revision": "715db399342e7260ae0e",
+    "url": "/react-portfolio/static/css/main.3ad73afb.chunk.css"
   },
   {
     "revision": "f9a898aaae0e449d770b",
     "url": "/react-portfolio/static/js/2.ab476efc.chunk.js"
   },
   {
-    "revision": "0b20932b29a01ec68f96",
-    "url": "/react-portfolio/static/js/main.e9272ed3.chunk.js"
+    "revision": "715db399342e7260ae0e",
+    "url": "/react-portfolio/static/js/main.1fafaef0.chunk.js"
   },
   {
     "revision": "94872cf7e81768d148a3",
